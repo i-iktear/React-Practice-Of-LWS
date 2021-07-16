85f4ad04043aa6f2905f37c49d52222318996297
i-iktear <emon5aurk@gmail.com>
2021-07-16 16:38:07 +0600

    Component for Composition Practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // import ClockList from "./components/ClockList";
 // import Form from "./components/Form";
 // import Calculator from "./components/Calculator";
-import Text from "./components/inheritance/Text";
+// import Text from "./components/inheritance/Text";
+import Text from "./components/composition/Text";
+import Emoji from "./components/composition/Emoji";
+import Bracket from "./components/composition/Bracket";
 
 function App() {
   // const quantity = [1, 2];
@@ -10,7 +13,16 @@ function App() {
       {/* <ClockList quantity={quantity} /> */}
       {/* <Form /> */}
       {/* <Calculator /> */}
-      <Text />
+
+      <Emoji>
+        {({ addEmoji }) => (
+          <Bracket>
+            {({ addBracket }) => (
+              <Text addEmoji={addEmoji} addBracket={addBracket} />
+            )}
+          </Bracket>
+        )}
+      </Emoji>
     </>
   );
 }
