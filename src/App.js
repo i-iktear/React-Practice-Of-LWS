f063b64031a990fc9959e324bf7946a816ff83d0
i-iktear <emon5aurk@gmail.com>
2021-07-28 15:51:41 +0600

    added: useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-import TodoFunctional from "./Functional Components/useState/TodoFunctional";
-import Todo from "./Functional Components/useState/Todo";
-import Counter from "./Functional Components/useState/Counter";
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Counter />
-        <Todo></Todo>
-        <TodoFunctional></TodoFunctional>
-      </>
-    );
-  }
+import React, { useState } from "react";
+// import MyComponentClass from "./Functional Components/useEffect/MyComponentClass";
+import MyComponentFunction from "./Functional Components/useEffect/MyComponentFunction";
+
+export default function App() {
+  const [show, setShow] = useState(true);
+  return (
+    <>
+      {/* <MyComponentClass /> */}
+      <div>{show && <MyComponentFunction />}</div>
+      <p>
+        <button onClick={() => setShow((prev) => !prev)}>
+          {" "}
+          {show ? "Hide component" : "Show component"}{" "}
+        </button>
+      </p>
+    </>
+  );
 }
