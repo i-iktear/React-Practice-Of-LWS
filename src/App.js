09aedf5f917b4baaeeb7ad583476e4915fbb0deb
i-iktear <emon5aurk@gmail.com>
2021-07-28 11:40:35 +0600

    Added: useStates in App file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import React from "react";
-import ClickCounter from "./components/L13: High order Component/ClickCounter";
-import Counter from "./components/L14: react render props/Counter";
-import Section from "./components/L16: React Context API/Section";
-import ThemeContext from "./components/L16: React Context API/Contexts/ThemeContexts";
+import TodoFunctional from "./Functional Components/useState/TodoFunctional";
+import Todo from "./Functional Components/useState/Todo";
+import Counter from "./Functional Components/useState/Counter";
 export default class App extends React.Component {
-  state = {
-    theme: "dark",
-  };
-
-  switchTheme = () => {
-    this.setState(({ theme }) => {
-      if (theme === "dark") {
-        return {
-          theme: "light",
-        };
-      } else {
-        return {
-          theme: "dark",
-        };
-      }
-    });
-  };
   render() {
-    const { theme } = this.state;
     return (
       <>
-        <Counter>
-          {(count, incrementCount) => (
-            <ClickCounter count={count} incrementCount={incrementCount} />
-          )}
-        </Counter>
-        <ThemeContext.Provider
-          value={{ theme: theme, switchTheme: this.switchTheme }}
-        >
-          <Section />
-        </ThemeContext.Provider>
+        <Counter />
+        <Todo></Todo>
+        <TodoFunctional></TodoFunctional>
       </>
     );
   }
