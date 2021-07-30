4a2bde760cfbe3049c006bfc382802883be83f02
i-iktear <emon5aurk@gmail.com>
2021-07-30 16:10:29 +0600

    useReducer hook Practice

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useReducer } from "react";
-import ComponentA from "./Functional Components/useReducer/ComponentA";
-
-export const counterContext = React.createContext();
-
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "increase":
-      return state + 1;
-
-    case "decrease":
-      return state - 1;
-
-    default:
-      return state;
-  }
-};
+import React from "react";
+import GetPost from "./Functional Components/useReducer/GetPost";
+import Getpost2 from "./Functional Components/useReducer/Getpost2";
 
 export default function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="app">
-      <div> Count: {count} </div>
-      <counterContext.Provider value={{ dispatch: dispatch }}>
-        <ComponentA />
-      </counterContext.Provider>
+      <GetPost />
+      <Getpost2 />
     </div>
   );
 }
