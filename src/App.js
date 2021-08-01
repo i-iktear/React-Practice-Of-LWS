863c489075b806a806de6182e48de00e75e34ec5
i-iktear <emon5aurk@gmail.com>
2021-08-01 16:21:31 +0600

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Functional Components/styled component/assets/css/dark.css";
+// import "./Functional Components/styled component/assets/css/dark.css";
 import Tutorial from "./Functional Components/styled component/Tutorial";
 
 export default function App() {
