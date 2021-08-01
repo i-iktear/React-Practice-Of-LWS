8299f51a372a46540ba4b74aa2b29176392c2e8b
i-iktear <emon5aurk@gmail.com>
2021-08-01 16:21:12 +0600

    Styled Components

--- src/Functional Components/styled component/Card.js
+++ src/Functional Components/styled component/Card.js
@@ -1,26 +1,29 @@
 import React from "react";
 import nerdImage from "./assets/images/illustration.png";
 import Button from "./Button";
-import Content from "./Content";
-import Image from "./Image";
-import Tag from "./Tag";
+import {
+  ButtonContainer,
+  CardContainer,
+  ContentContainer,
+} from "./styles/Container.styles";
+import { H1, P, Tag, Image } from "./styles/Elements";
 const Card = () => {
   return (
-    <div className="card">
-      <Content>
-        <Tag text="exclusive"></Tag>
-        <h1>React Styled Components</h1>
-        <p>
+    <CardContainer>
+      <ContentContainer>
+        <Tag color="#fff"> Exclusive</Tag>
+        <H1>React Styled Components</H1>
+        <P>
           Exclusive React JS Tutorial on Styled Components where you will learn
           why we need this & how to use it.
-        </p>
-        <div className="buttons">
+        </P>
+        <ButtonContainer>
           <Button link="https://lwsbd.link/rsc" text="Watch now" />
           <Button link="https://lwsbd.link/react" text="Github repo" />
-        </div>
-      </Content>
-      <Image imgSrc={nerdImage} altTag="Nerd" width="300px" />
-    </div>
+        </ButtonContainer>
+      </ContentContainer>
+      <Image src={nerdImage} alt="Nerd" width="300px" />
+    </CardContainer>
   );
 };
 
