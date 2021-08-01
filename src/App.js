4d0dda23e3cf3c549af47f6c0f45578ab261aefc
i-iktear <emon5aurk@gmail.com>
2021-08-01 16:29:08 +0600

    Styled Component Practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import "./Functional Components/styled component/assets/css/dark.css";
 import Tutorial from "./Functional Components/styled component/Tutorial";
+import GlobalStyle from "./Functional Components/styled component/styles/Global.styles";
 
 export default function App() {
   return (
     <div className="app">
+      <GlobalStyle />
       <Tutorial />
     </div>
   );
